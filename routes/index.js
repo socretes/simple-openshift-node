4c4e7add7551c4dbfd75430eab2e31016b5cafa6
Andy Sharp <andysharp2003@yahoo.com>
2020-11-04 23:05:29 +0000

    wrote hostname

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var os = require("os");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/routes', function(req, res, next) {
+  res.send(os.hostname());
 });
 
 module.exports = router;
